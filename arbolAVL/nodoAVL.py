02d4418b6d02da5b0cd462914a0598448db7d067
Sergio Larios <68086604+sa-larios22@users.noreply.github.com>
2023-10-06 15:14:22 -0600

    Delete arbolAVL directory

--- arbolAVL/nodoAVL.py
+++
@@ -1,7 +0,0 @@
-class Nodo_AVL():
-    def __init__(self, valor):
-        self.valor = valor
-        self.izquierdo = None
-        self.derecho = None
-        self.altura = 1
-        self.factor_equilibrio = 0
