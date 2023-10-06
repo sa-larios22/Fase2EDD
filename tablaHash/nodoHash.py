e08c20409aa0711252214ae538364e1fbcd27caf
Sergio Larios <68086604+sa-larios22@users.noreply.github.com>
2023-10-06 15:14:30 -0600

    Delete tablaHash directory

--- tablaHash/nodoHash.py
+++
@@ -1,6 +0,0 @@
-class NodoHash():
-    def __init__(self, codigo, nombre, password, puesto):
-        self.codigo = codigo
-        self.nombre = nombre
-        self.password = password
-        self.puesto = puesto
